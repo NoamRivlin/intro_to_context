13ecc051d86ee92fedf0d435f898c60ee7d15a6f
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-10-27 08:43:19 -0600

    UPdate BlogCard

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -1,14 +1,16 @@
-import Card from "react-bootstrap/Card";
+import { Card, Button } from "react-bootstrap";
 import { toTitleCase, toSentenceCase } from "../../utils";
-const BlogCard = ({ userId, id, title, body }) => {
+const BlogCard = ({ blogPost }) => {
+  const { userId, title, body } = blogPost;
   return (
-    <Card className="m-1" style={{ width: "18rem" }}>
+    <Card className="blog-card">
       <Card.Body>
         <Card.Title>{toTitleCase(title)}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
           User Number {userId}
         </Card.Subtitle>
         <Card.Text>{toSentenceCase(body)}</Card.Text>
+        <Button variant="primary">"Add to Favs"</Button>
       </Card.Body>
     </Card>
   );
